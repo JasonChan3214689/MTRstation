84cdd1ac59530a2ee8c3f79fda3b64336d309961
JasonChan3214689 <148197517+JasonChan3214689@users.noreply.github.com>
2023-12-02 20:15:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,384 @@
+line = {
+  KTL: {
+    text: "觀塘線",
+    color: "#7ed957",
+    sta: [
+      { code: "WHA", name: "Whampoa", cname: "黃埔" },
+      { code: "HOM", name: "Ho Man Tin", cname: "何文田" },
+      { code: "YMT", name: "Yau Ma Tei", cname: "油麻地" },
+      { code: "MOK", name: "Mong Kok", cname: "旺角" },
+      { code: "PRE", name: "Prince Edward", cname: "太子" },
+      { code: "SKM", name: "Shek Kip Mei", cname: "石硤尾" },
+      { code: "KOT", name: "Kowloon Tong", cname: "九龍塘" },
+      { code: "LOF", name: "Lok Fu", cname: "樂富" },
+      { code: "WTS", name: "Wong Tai Sin", cname: "黃大仙" },
+      { code: "DIH", name: "Diamond Hill", cname: "鑽石山" },
+      { code: "CHH", name: "Choi Hung", cname: "彩虹" },
+      { code: "KOB", name: "Kowloon Bay", cname: "九龍灣" },
+      { code: "NTK", name: "Ngau Tau Kok", cname: "牛頭角" },
+      { code: "KWT", name: "Kwun Tong", cname: "觀塘" },
+      { code: "LAT", name: "Lam Tin", cname: "藍田" },
+      { code: "YAT", name: "Yau Tong", cname: "油塘" },
+      { code: "TIK", name: "Tiu Keng Leng", cname: "調景嶺" },
+    ],
+  },
+  ISL: {
+    text: "港島線",
+    color: "#004aad",
+    sta: [
+      { code: "KET", name: "Kennedy Town", cname: "堅尼地城" },
+      { code: "HKU", name: "HKU", cname: "香港大學" },
+      { code: "SYP", name: "Sai Ying Pun", cname: "西營盤" },
+      { code: "SHW", name: "Sheung Wan", cname: "上環" },
+      { code: "CEN", name: "Central", cname: "中環" },
+      { code: "ADM", name: "Admiralty", cname: "金鐘" },
+      { code: "WAC", name: "Wan Chai", cname: "灣仔" },
+      { code: "CAB", name: "Causeway Bay", cname: "銅鑼灣" },
+      { code: "TIH", name: "Tin Hau", cname: "天后" },
+      { code: "FOH", name: "Fortress Hill", cname: "炮台山" },
+      { code: "NOP", name: "North Point", cname: "北角" },
+      { code: "QUB", name: "Quarry Bay", cname: "鰂魚涌" },
+      { code: "TAK", name: "Tai Koo", cname: "太古" },
+      { code: "SWH", name: "Sai Wan Ho", cname: "西灣河" },
+      { code: "SKW", name: "Shau Kei Wan", cname: "筲箕灣" },
+      { code: "HFC", name: "Heng Fa Chuen", cname: "杏花邨" },
+      { code: "CHW", name: "Chai Wan", cname: "柴灣" },
+    ],
+  },
+  TWL: {
+    text: "荃灣線",
+    color: "#ff3131",
+    sta: [
+      { code: "CEN", name: "Central", cname: "中環" },
+      { code: "ADM", name: "Admiralty", cname: "金鐘" },
+      { code: "TST", name: "Tsim Sha Tsui", cname: "尖沙咀" },
+      { code: "JOR", name: "Jordan", cname: "佐敦" },
+      { code: "YMT", name: "Yau Ma Tei", cname: "油麻地" },
+      { code: "MOK", name: "Mong Kok", cname: "旺角" },
+      { code: "CEN", name: "Central", cname: "中環" },
+      { code: "ADM", name: "Admiralty", cname: "金鐘" },
+      { code: "TST", name: "Tsim Sha Tsui", cname: "尖沙咀" },
+      { code: "JOR", name: "Jordan", cname: "佐敦" },
+      { code: "YMT", name: "Yau Ma Tei", cname: "油麻地" },
+      { code: "MOK", name: "Mong Kok", cname: "旺角" },
+    ],
+  },
+  SIL: {
+    text: "南港島線",
+    color: "#cbcd09",
+    sta: [
+      { code: "ADM", name: "Admiralty", cname: "金鐘" },
+      { code: "OCP", name: "Ocean Park", cname: "海洋公園" },
+      { code: "WCH", name: "Wong Chuk Hang", cname: "黃竹坑" },
+      { code: "LET", name: "Lei Tung", cname: "利東" },
+      { code: "SOH", name: "South Horizons", cname: "海怡半島" },
+    ],
+  },
+  TCL: {
+    text: "東涌線",
+    color: "#f6943d",
+    sta: [
+      { code: "HOK", name: "Hong Kong", cname: "香港" },
+      { code: "KOW", name: "Kowloon", cname: "九龍" },
+      { code: "OLY", name: "Olympic", cname: "奧運" },
+      { code: "NAC", name: "Nam Cheong", cname: "南昌" },
+      { code: "LAK", name: "Lai King", cname: "荔景" },
+      { code: "TSY", name: "Tsing Yi", cname: "青衣" },
+      { code: "SUN", name: "Sunny Bay", cname: "欣澳" },
+      { code: "TUC", name: "Tung Chung", cname: "東涌" },
+    ],
+  },
+  EAL: {
+    text: "東鐵線",
+    color: "#5ce1e6",
+    sta: [
+      { code: "ADM", name: "Admiralty", cname: "金鍾" },
+      { code: "EXC", name: "Exhibition Centre", cname: "會展" },
+      { code: "HUH", name: "Hung Hom", cname: "紅磡" },
+      { code: "MKK", name: "Mong Kok East", cname: "旺角東" },
+      { code: "KOT", name: "Kowloon Tong", cname: "九龍塘" },
+      { code: "TAW", name: "Tai Wai", cname: "大圍" },
+      { code: "SHT", name: "Sha Tin", cname: "沙田" },
+      { code: "FOT", name: "Fo Tan", cname: "火炭" },
+      { code: "RAC", name: "Racecourse", cname: "馬場" },
+      { code: "UNI", name: "University", cname: "大學" },
+      { code: "TAP", name: "Tai Po Market", cname: "大埔墟" },
+      { code: "TWO", name: "Tai Wo", cname: "太和" },
+      { code: "FAN", name: "Fanling", cname: "粉嶺" },
+      { code: "SHS", name: "Sheung Shui", cname: "上水" },
+      { code: "LOW", name: "Lo Wu", cname: "羅湖" },
+      { code: "LMC", name: "Lok Ma Chau", cname: "落馬洲" },
+    ],
+  },
+  TML: {
+    text: "屯馬線",
+    color: "#8d6019",
+    sta: [
+      { code: "WKS", name: "Wu Kai Sha", cname: "烏溪沙" },
+      { code: "MOS", name: "Ma On Shan", cname: "馬鞍山" },
+      { code: "HEO", name: "Heng On", cname: "恆安" },
+      { code: "TSH", name: "Tai Shui Hang", cname: "大水坑" },
+      { code: "SHM", name: "Shek Mun", cname: "石門" },
+      { code: "CIO", name: "City One", cname: "第一城" },
+      { code: "STW", name: "Sha Tin Wai", cname: "沙田圍" },
+      { code: "CKT", name: "Che Kung Temple", cname: "車公廟" },
+      { code: "TAW", name: "Tai Wai", cname: "大圍" },
+      { code: "HIK", name: "Hin Keng", cname: "顯徑" },
+      { code: "DIH", name: "Diamond Hill", cname: "鑽石山" },
+      { code: "KAT", name: "Kai Tak", cname: "啟德" },
+      { code: "SUW", name: "Sung Wong Toi", cname: "宋皇臺" },
+      { code: "TKW", name: "To Kwa Wan", cname: "土瓜灣" },
+      { code: "HOM", name: "Ho Man Tin", cname: "何文田" },
+      { code: "HUH", name: "Hung Hom", cname: "紅磡" },
+      { code: "ETS", name: "East Tsim Sha Tsui", cname: "尖東" },
+      { code: "AUS", name: "Austin", cname: "柯士甸" },
+      { code: "NAC", name: "Nam Cheong", cname: "南昌" },
+      { code: "MEF", name: "Mei Foo", cname: "美孚" },
+      { code: "TWW", name: "Tsuen Wan West", cname: "荃灣西" },
+      { code: "KSR", name: "Kam Sheung Road", cname: "錦上路" },
+      { code: "YUL", name: "Yuen Long", cname: "元朗" },
+      { code: "LOP", name: "Long Ping", cname: "朗屏" },
+      { code: "TIS", name: "Tin Shui Wai", cname: "天水圍" },
+      { code: "SIH", name: "Siu Hong", cname: "兆康" },
+      { code: "TUM", name: "Tuen Mun", cname: "屯門" },
+    ],
+  },
+  AEL: {
+    text: "機場快線",
+    color: "#3d9ea0",
+    sta: [
+      { code: "HOK", name: "Hong Kong", cname: "香港" },
+      { code: "KOW", name: "Kowloon", cname: "九龍" },
+      { code: "TSY", name: "Tsing Yi", cname: "青衣" },
+      { code: "AIR", name: "Airport", cname: "機場" },
+      { code: "AWE", name: "AsiaWorld Expo", cname: "博覽館" },
+    ],
+  },
+  TKL: {
+    text: "將軍澳線",
+    color: "#8d45ab",
+    sta: [
+      { code: "NOP", name: "North Point", cname: "北角" },
+      { code: "QUB", name: "Quarry Bay", cname: "鰂魚涌" },
+      { code: "YAT", name: "Yau Tong", cname: "油塘" },
+      { code: "TIK", name: "Tiu Keng Leng", cname: "調景嶺" },
+      { code: "TKO", name: "Tseung Kwan O", cname: "將軍澳" },
+      { code: "LHP", name: "LOHAS Park", cname: "康城" },
+      { code: "HAH", name: "Hang Hau", cname: "坑口" },
+      { code: "POA", name: "Po Lam", cname: "寶琳" },
+    ],
+  },
+};
+
+let tclButton = document.querySelector("div.東涌");
+let ktlButton = document.querySelector("div.觀塘");
+let erlButton = document.querySelector("div.東鐵");
+let islButton = document.querySelector("div.港島");
+let twlButton = document.querySelector("div.荃灣");
+let silButton = document.querySelector("div.南港島");
+let tmlButton = document.querySelector("div.屯馬線");
+let aelButton = document.querySelector("div.機場快線");
+let tklButton = document.querySelector("div.將軍澳線");
+
+const fetchMTRData = async (buttonId) => {
+  const MTRdata = {};
+  let buttonSelect = select(buttonId);
+  console.log(buttonSelect);
+  for (const station of line[buttonSelect].sta) {
+    const res = await fetch(
+      `https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=${buttonSelect}&sta=${station.code}`
+    );
+    const results = await res.json();
+    MTRdata[station.code] = results;
+  }
+  updateTime(MTRdata);
+  getFirstLocatin(buttonSelect);
+  genCardUP(MTRdata, buttonSelect);
+  genCardDOWN(MTRdata, buttonSelect);
+};
+
+function select(buttonId) {
+  if (buttonId === "ktlButton") {
+    return "KTL";
+  } else if (buttonId === "tclButton") {
+    return "TCL";
+  } else if (buttonId === "erlButton") {
+    return "EAL";
+  } else if (buttonId === "twlButton") {
+    return "TWL";
+  } else if (buttonId === "islButton") {
+    return "ISL";
+  } else if (buttonId === "silButton") {
+    return "SIL";
+  } else if (buttonId === "tmlButton") {
+    return "TML";
+  } else if (buttonId === "aelButton") {
+    return "AEL";
+  } else if (buttonId === "tklButton") {
+    return "TKL";
+  }
+}
+
+tclButton.addEventListener("click", () => {
+  fetchMTRData("tclButton");
+});
+
+ktlButton.addEventListener("click", () => {
+  fetchMTRData("ktlButton");
+});
+
+erlButton.addEventListener("click", () => {
+  fetchMTRData("erlButton");
+});
+
+islButton.addEventListener("click", () => {
+  fetchMTRData("islButton");
+});
+
+twlButton.addEventListener("click", () => {
+  fetchMTRData("twlButton");
+});
+
+silButton.addEventListener("click", () => {
+  fetchMTRData("silButton");
+});
+
+tmlButton.addEventListener("click", () => {
+  fetchMTRData("tmlButton");
+});
+
+aelButton.addEventListener("click", () => {
+  fetchMTRData("aelButton");
+});
+
+tklButton.addEventListener("click", () => {
+  fetchMTRData("tklButton");
+});
+
+function updateTime(MTRdata) {
+  let currentTime;
+  let LastUpdateWrapper = document.querySelector(".LastUpdateWrapper");
+  LastUpdateWrapper.innerHTML = "";
+  for (const key in MTRdata) {
+    currentTime = MTRdata[key].curr_time;
+    break;
+  }
+  LastUpdateContent = `<p class="LastUpdate">最後更新時間:${currentTime}</p>`;
+  LastUpdateWrapper.innerHTML += LastUpdateContent;
+}
+
+function getFirstLocatin(buttonSelect) {
+  console.log(buttonSelect);
+  let locationWrapper = document.querySelector(".locationWrapper");
+  locationWrapper.style.display = "flex";
+  const firstKey = line[buttonSelect]["sta"][0].cname;
+  const lastKey =
+    line[buttonSelect]["sta"][line[buttonSelect]["sta"].length - 1].cname;
+  let location1Text = document.querySelector(".location1Text");
+  let location2Text = document.querySelector(".location2Text");
+  location2Text.innerText = firstKey;
+  location1Text.innerText = lastKey;
+}
+
+function genCardUP(MTRdata, buttonSelect) {
+  resultObj1 = {};
+  let locationCardHolderUP = document.querySelector(".locationCardHolderUP");
+  locationCardHolderUP.innerHTML = "";
+
+  for (const key in MTRdata) {
+    resultObj1[key] = MTRdata[key].data;
+  }
+
+  for (const key in resultObj1) {
+    let items = Object.keys(resultObj1[key]);
+    for (const item of items) {
+      if (resultObj1[key][item].UP) {
+        let upArray = resultObj1[key][item].UP[0];
+        if (resultObj1[key][item].UP[0]) {
+          let arrTime = upArray.time;
+          let arrPlatform = upArray.plat;
+
+          const stations = line[buttonSelect].sta;
+
+          const stationCode = item.split("-")[1];
+
+          const station = stations.find(
+            (station) => station.code === stationCode
+          );
+          const stationName = station ? station.cname : "";
+
+          let cardContent = `
+          <div class="cardDown">
+            <h1 class="cardLocation">${stationName}</h1>
+            <h5 class="cardTime">開出時間: ${arrTime}</h5>
+            <h5 class="cardPlatform">開出月台:${arrPlatform}</h5>
+          </div>
+        `;
+
+          const cardColor = line[buttonSelect].color;
+          console.log(cardColor);
+          locationCardHolderUP.innerHTML += cardContent;
+          let cardDownList = document.querySelectorAll(".cardDown");
+          cardDownList.forEach((element) => {
+            element.style.backgroundColor = cardColor;
+          });
+        }
+      }
+    }
+  }
+}
+
+function genCardDOWN(MTRdata, buttonSelect) {
+  resultObj1 = {};
+  let locationCardHolderDOWN = document.querySelector(
+    ".locationCardHolderDOWN"
+  );
+  let cardContents = [];
+
+  for (const key in MTRdata) {
+    resultObj1[key] = MTRdata[key].data;
+  }
+
+  for (const key in resultObj1) {
+    let items = Object.keys(resultObj1[key]);
+    for (const item of items) {
+      if (resultObj1[key][item].DOWN) {
+        let downArray = resultObj1[key][item].DOWN[0];
+        //console.log(downArray);
+        if (resultObj1[key][item].DOWN[0]) {
+          let arrTime = downArray.time;
+          let arrPlatform = downArray.plat;
+
+          const stations = line[buttonSelect].sta;
+
+          const stationCode = item.split("-")[1];
+
+          const station = stations.find(
+            (station) => station.code === stationCode
+          );
+          const stationName = station ? station.cname : "";
+
+          let cardContent = `
+          <div class="cardDown">
+            <h1 class="cardLocation">${stationName}</h1>
+            <h5 class="cardTime">開出時間: ${arrTime}</h5>
+            <h5 class="cardPlatform">開出月台:${arrPlatform}</h5>
+          </div>
+        `;
+
+          cardContents.push(cardContent);
+        }
+      }
+    }
+  }
+
+  cardContents.reverse();
+
+  locationCardHolderDOWN.innerHTML = cardContents.join("");
+  const cardColor = line[buttonSelect].color;
+  console.log(cardColor);
+  let cardDownList = document.querySelectorAll(".cardDown");
+  cardDownList.forEach((element) => {
+    element.style.backgroundColor = cardColor;
+  });
+}
